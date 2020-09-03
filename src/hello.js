61ad57340656231e462c2122c3010555d7643163
Jun Villa <junarvi@yahoo.ca>
2020-09-03 10:37:19 -0400

    asdf

--- src/hello.js
+++ src/hello.js
@@ -1,13 +1,7 @@
 module.exports = (pluginContext) => {
   return (name, env = {}) => {
     return new Promise((resolve, reject) => {
-      resolve([
-        {
-          icon: 'fa-hand-spock-o',
-          title: `Hello, ${name}!`,
-          subtitle: 'Please type your name!',
-        },
-      ])
+      resolve("asdflkjasdflkj")
     })
   }
 }
